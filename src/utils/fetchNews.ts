4745679cc1d513078aeab1351b7b29aaed02e71e
Ramyashree034 <151832023+Ramyashree034@users.noreply.github.com>
2026-02-27 19:53:47 +0530

    Update fetchNews.ts

--- src/utils/fetchNews.ts
+++ src/utils/fetchNews.ts
@@ -1,17 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "49fd4dac4eae491984cb25ee84d7cdc7";
-
 export async function fetchNews(category: string) {
-  const res = await axios.get(
-    `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=${API_KEY}`
-  );
-
-  return res.data.articles.map((a: any, index: number) => ({
-    id: index.toString(),
-    title: a.title,
-    description: a.description,
-    image: a.urlToImage,
-    source: a.source.name,
-  }));
+  const res = await axios.get(`/api/news?category=${category}`);
+  return res.data;
 }
